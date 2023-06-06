dcf6559929d7f5da510734c7632e4b7d5d3c9456
Rishitha T <rishitha6399@gmail.com>
2023-06-06 16:09:34 +0530

    To do list commit

---
+++ task.js
@@ -0,0 +1,33 @@
+function addMore() {
+    document.getElementById('error').innerHTML="";
+    let name=document.getElementById('text').value;
+    if(name==''){
+        document.getElementById('error').innerHTML="please enter value";
+    }else{
+        let box=document.getElementById('box');
+
+        let li=document.createElement('li');
+        li.textContent=name;
+        let a=document.createElement('a');
+        a.textContent="x";
+        a.href="javascript:void(0)";
+        a.className="remove";
+        li.appendChild(a);
+        
+        let pos=box.firstElementChild;
+        
+        if(pos==null){
+            box.appendChild(li);
+        }else{
+            box.insertBefore(li,pos);
+        }
+    }
+    document.getElementById('text').value="";
+}
+
+let btn=document.querySelector('ul');
+btn.addEventListener('click',function(e){
+    let box=document.getElementById('box');
+    let li=e.target.parentNode;
+    box.removeChild(li);
+});
